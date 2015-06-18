23e6a81597727cced05e7581d3fb0c1df7281e5e
regimental12 <richard.hankins@gmail.com>
2015-06-18 23:12:30 +0100

    pause menu half way

---
+++ Assets/PauseButton.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseButton : MonoBehaviour {
+
+
+    public GameObject ResumeButton;
+    //public GameObject musicButton;
+    //public GameObject quitButton;
+    GameObject resumeClone/*, musicClone, quitClone*/;
+    
+    public bool paused;
+    public bool noGUI = false;
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        if(noGUI)
+        {
+            destroyGUI();
+            Time.timeScale = 1.0f;
+        }
+	}
+
+    void OnMouseDown()
+    {
+        togglePause();
+        if (paused)
+        {
+            Time.timeScale = 0.0f;
+            
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
+        
+    }
+
+    bool togglePause()
+    {
+        if(paused == false)
+        {
+            noGUI = false;
+            buildGUI();
+            return paused = true;
+        }
+        else 
+        {
+            return paused = false;
+        }
+    }
+
+    void buildGUI()
+    {
+        
+        resumeClone = Instantiate(ResumeButton.transform, ResumeButton.transform.position, ResumeButton.transform.rotation) as GameObject;
+        /*musicClone = Instantiate(musicButton.transform, musicButton.transform.position, musicButton.transform.rotation) as GameObject;
+        quitClone = Instantiate(quitButton.transform, quitButton.transform.position, quitButton.transform.rotation) as GameObject;*/
+    }
+
+    void destroyGUI()
+    {
+        Destroy(resumeClone.gameObject);
+        /*DestroyImmediate(musicButton , true);
+        DestroyImmediate(quitButton , true);*/
+    }
+
+    
+}
---
+++ Assets/resumeButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class resumeButton : MonoBehaviour {
+
+    public GameObject pb;
+
+	// Use this for initialization
+	void Start () {
+
+        pb = GameObject.Find("PauseButton");
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnMouseDown()
+    {
+        pb.GetComponent<PauseButton>().paused = false;
+        pb.GetComponent<PauseButton>().noGUI = true;
+        Time.timeScale = 1.0f;
+        //Destroy(this.gameObject);
+    }
+}
